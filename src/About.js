5ec15fed473f56c02e0af1551d77f131a8d2de87
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-07-11 16:07:25 +0200

    Changed styling of Homepage and initial information of the About section

--- src/About.js
+++ src/About.js
@@ -1,6 +1,33 @@
-//import Photo from "./images/laetitiasaunderspaintingphoto.jpg";
 import "./About.css";
 
 export default function Header() {
-  return;
+  return (
+    <div className="About">
+      <div className="about-section text-section">
+        <h2>About me</h2>
+        <p>
+          <span>
+            A brief overview of my inspiring journey to becoming a front-end
+            developer:
+          </span>
+        </p>
+        <p>
+          I am a 28-year-old woman from France who has taken an inspiring
+          journey to discover my true passion in life. While many of my peers
+          went off to university with clear goals, I explored various subjects,
+          striving to find my own path. Although my initial university studies
+          didn't resonate with me, and I eventually chose to work as a waitress,
+          this period taught me resilience and adaptability. When COVID-19 hit,
+          the hotel where I worked unfortunately closed. This setback gave me an
+          opportunity to rethink my career. Through a support service at the
+          unemployment centre, I connected with a professional who was dedicated
+          to helping me uncover a fulfilling career path. Her guidance was
+          invaluable, and after many months, I discovered my passion for
+          front-end development. Now, I am excited to pursue this new and
+          exciting career.
+        </p>
+      </div>
+      <div className="about-section"></div>
+    </div>
+  );
 }
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,25 +1,33 @@
 import "./Homepage.css";
+import Photo from "./images/laetitiasaunderspaintingphoto.jpg";
 
 export default function Homepage() {
   return (
     <div className="Homepage">
-      <h1>Junior Front-End- Developer</h1>
-      <h3>
-        Hi, I'm <span>Laetitia</span>!
-      </h3>
-      <p>
-        Front-end developer passionate about creating interactive applications
-        and experiences on the web.
-      </p>
-      <p>
-        Welcome to my portfolio website! Here, you'll discover a showcase of my
-        projects, gain insights into who I am, and find ways to connect with me.
-        Take a journey through my work to witness firsthand the projects I've
-        poured my passion into. Whether you're curious about my coding
-        endeavors, eager to learn more about my background, or simply wish to
-        reach out, you'll find everything you need right here! This website was
-        designed and coded by me.
-      </p>
+      <div className="container">
+        <div className="homepage-section">
+          <h1>Junior Front-End- Developer</h1>
+          <h3>
+            Hi, I'm <span>Laetitia</span>!
+          </h3>
+          <p>
+            Front-end developer passionate about creating interactive
+            applications and experiences on the web.
+          </p>
+          <p>
+            Welcome to my portfolio website! Here, you'll discover a showcase of
+            my projects, gain insights into who I am, and find ways to connect
+            with me. Take a journey through my work to witness firsthand the
+            projects I've poured my passion into. Whether you're curious about
+            my coding endeavors, eager to learn more about my background, or
+            simply wish to reach out, you'll find everything you need right
+            here! This website was designed and coded by me.
+          </p>
+        </div>
+        <div className="homepage-section">
+          <img src={Photo} alt="Artistic portrait of Laetitia" />
+        </div>
+      </div>
     </div>
   );
 }
