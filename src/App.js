66f242c2efca41f0f7a6b07e1f4b8268efa5782b
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-07-11 15:01:07 +0200

    Created and styled homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import "./App.css";
 import Navbar from "./Navbar";
-import Header from "./Header";
+import Homepage from "./Homepage";
 import About from "./About";
 import Projects from "./Projects";
+import Contact from "./Contact";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
-        <section className="header-section">
-          <Header />
-        </section>
-        <main>
-          <section className="first-main-section">
-            <About />
-          </section>
-          <section className="second-main-section">
-            <Projects />
-          </section>
-        </main>
       </header>
+      <main className="App-main">
+        <section className="homepage-section">
+          <Homepage />
+        </section>
+        <section className="about-section">
+          <About />
+        </section>
+        <section className="projects-section">
+          <Projects />
+        </section>
+        <section className="contact-section">
+          <Contact />
+        </section>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ src/Homepage.js
@@ -0,0 +1,25 @@
+import "./Homepage.css";
+
+export default function Homepage() {
+  return (
+    <div className="Homepage">
+      <h1>Junior Front-End- Developer</h1>
+      <h3>
+        Hi, I'm <span>Laetitia</span>!
+      </h3>
+      <p>
+        Front-end developer passionate about creating interactive applications
+        and experiences on the web.
+      </p>
+      <p>
+        Welcome to my portfolio website! Here, you'll discover a showcase of my
+        projects, gain insights into who I am, and find ways to connect with me.
+        Take a journey through my work to witness firsthand the projects I've
+        poured my passion into. Whether you're curious about my coding
+        endeavors, eager to learn more about my background, or simply wish to
+        reach out, you'll find everything you need right here! This website was
+        designed and coded by me.
+      </p>
+    </div>
+  );
+}
